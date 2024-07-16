c4c7708cde7e87bed1f84fa95f08859e8e62e56c
Marvelco <Marvelcodev@gmail.com>
2024-07-16 20:46:21 +0300

    what was i thinking back then

--- src/model/user.js
+++ src/model/user.js
@@ -1,81 +1,21 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema(
-  {
-    created: {
-      type: Date,
-      required: true,
-      default: Date.now,
+const UserSchema = new mongoose.Schema(
+    {
+        created: { type: Date, required: true },
+        banned: { type: Boolean, default: false },
+        discordId: { type: String, required: true, unique: true },
+        accountId: { type: String, required: true, unique: true },
+        username: { type: String, required: true, unique: true },
+        username_lower: { type: String, required: true, unique: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true }
     },
-    banned: {
-      type: Boolean,
-      default: false,
-    },
-    discordId: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    accountId: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      select: false, // Exclude password by default when querying
-    },
-  },
-  {
-    collection: "users",
-    timestamps: true,
-  }
-);
+    {
+        collection: "users"
+    }
+)
 
-// Virtual field for full name
-userSchema.virtual("fullName").get(function () {
-  return `${this.firstName} ${this.lastName}`;
-});
+const model = mongoose.model('UserSchema', UserSchema);
 
-// Pre-save hook to hash the password before saving
-userSchema.pre("save", async function (next) {
-  // Only hash the password if it's modified or new
-  if (!this.isModified("password")) {
-    return next();
-  }
-
-  try {
-    // Generate a salt
-    const salt = await bcrypt.genSalt(10);
-
-    // Hash the password using the generated salt
-    const hashedPassword = await bcrypt.hash(this.password, salt);
-
-    // Set the hashed password back to the model
-    this.password = hashedPassword;
-
-    next();
-  } catch (error) {
-    return next(error);
-  }
-});
-
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
-
+module.exports = model;
